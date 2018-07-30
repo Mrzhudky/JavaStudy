29e37bd6a77ea32220099d8fd125b2d517b360e1
Mrzhudky <1183260441@qq.com>
2018-07-30 21:35:33 +0800

    helloIDEA

---
+++ StudyWithViode/hello-idea/src/com/funtl/hello/idea/MySolution.java
@@ -0,0 +1,10 @@
+package com.funtl.hello.idea;
+
+/**
+ * Created by zhu on 2018/7/19.
+ */
+public class MySolution {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA");
+    }
+}
